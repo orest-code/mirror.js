ab06ac345615991b0884e7848bd2ac6fc8d06a35
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-05 18:35:28 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+
 const { Client, RichEmbed } = require('discord.js');
 const fs = require("fs");
 const newUsers = [];
