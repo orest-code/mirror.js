65b7119bb274a67f5628d903b98115cadf445417
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-05 22:21:25 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -98,7 +98,7 @@ client.on("message", async message => {
       .setTitle('Группа ВКонтакте')
       .setURL('https://vk.com/mirrords')
       .setImage('https://cdn.discordapp.com/icons/521249346050850816/1aa2c052174d4f332855a9440c994bc2.png')
-    message.channel.send(exampleEmbed);
+    message.channel.send(embed);
   }
     if(command === "telegram") {
     message.channel.send('**Наша группа Телеграмм - https://t.me/mirrords**')
