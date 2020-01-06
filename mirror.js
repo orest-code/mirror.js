367950af91365e02d641440750401a5eaf8f6f3a
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-06 14:07:57 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -64,7 +64,7 @@ client.on("message", async message => {
   //random number command
     if(command === "random") {
     var random = Math.floor(Math.random() *100)+1;
-    message.channel.send("random");
+    message.channel.send(random);
     }
 
   //serverinfo command
