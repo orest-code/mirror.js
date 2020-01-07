76de3d2de82113964b9d9aa36d59273850b6b1c4
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-07 18:47:18 +0200

    Delete prefix.js

--- commands/prefix.js
+++
@@ -1,7 +0,0 @@
-
-module.exports.run = async (bot, message, args, database) => {
-	if (args[0]) {
-		database.getGuildData(msg.guild).prefix = args[0];
-		await message.channel.send(`Новый префикс для команд: '${args[0]}'`);
-	}
-}
