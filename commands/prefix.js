2b62dff74588348918ec4e0e83547b33b5d55917
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-07 18:36:42 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,1 +1,7 @@
 
+module.exports.run = async (bot, message, args, database) => {
+	if (args[0]) {
+		database.getGuildData(msg.guild).prefix = args[0];
+		await message.channel.send(`Новый префикс для команд: '${args[0]}'`);
+	}
+}
