c44c72337d8a904549617ba6065cf2f35f1795a1
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-07 18:39:20 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -105,9 +105,9 @@ client.on("message", async message => {
   
   if(message.author.bot) return;
   
-  if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+  if(message.content.indexOf(prefix) !== 0) return;
   
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   //help command
