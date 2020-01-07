1e676a6dc0765a14c492cc115872e43b5d2e718d
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-07 17:58:26 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -77,7 +77,7 @@ client.on("message", async message => {
    var role = message.guild.roles.find(role => role.name === "Vip");
    message.member.addRole(role);
    message.delete().catch(O_o=>{}); 
-   message.channel.send("**Вы получили роль**. ``" + role.name + "`` **✓**").then(function(message) {
+   message.channel.send("**Вы получили роль**  ``" + role.name + "``  **✓**").then(function(message) {
     message.delete(3000);
   });
   }
