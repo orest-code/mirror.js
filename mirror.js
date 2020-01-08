5fc035bb33a17419924f6b9ca2d809017c8c633b
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-08 11:59:27 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { Client, RichEmbed } = require('discord.js');
+const { Client, RichEmbed } = require('discord.js'); 
+const config = require('./config.json');
+const fs = require("fs");
+let db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
 const fs = require("fs");
 const newUsers = [];
 const ms = require("ms");
-const prefix = "/";
 
 client.login(process.env.MIRROR);
 
@@ -46,9 +48,9 @@ client.on("message", async message => {
   
   if(message.author.bot) return;
   
-  if(message.content.indexOf(prefix) !== 0) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
   
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   //help command
