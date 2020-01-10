81ed5d6860e52d76b6a25d0b7f446db9b7360d45
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-01-10 20:16:06 +0200

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -51,7 +51,7 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  var mutedrole = message.guild.roles.find("Muted"); 
+  var mutedrole = message.guild.roles.find("mute", "Muted"); 
 
 
   //help command
