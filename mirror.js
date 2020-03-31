f5e79421444ebf3aa4cab36e697240ff968f2cad
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-03-31 22:14:11 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -54,29 +54,6 @@ client.on('message', message => {
   }
 }); 
 
-//welcome-leave message
-client.on("guildMemberAdd", member => {
-    var channel = bot.channels.get(annoncechannel)
-    let embed = new Discord.RichEmbed()
-    .setTitle(member.user.username+ " Vient de rejoindre le serveur !")
-    .setDescription("N'hésitez pas à lui souhaiter la bienvenue !")
-    .addField("Il y'a maintenant "+member.guild.members.size+" membres sur le serveur", "merci à vous !")
-    .setColor("GREEN")
-    .setThumbnail(member.user.displayAvatarURL)
-    channel.send(embed)
-});
-
-client.on("guildMemberRemove", member =>{
-    var channel = bot.channels.get(annoncechannel)
-    let embed = new Discord.RichEmbed()
-    .setTitle(member.user.username+ " vient de quitter le serveur.")
-    .setDescription('On espère le revoir bientôt')
-    .addField("Il y'a maintenant "+member.guild.members.size+ " membres sur le serveur", "merci à vous ! ")
-    .setColor("RED")
-    .setThumbnail(member.user.displayAvatarURL)
-    channel.send(embed)
-})
-
 
 //commands
 client.on("message", async message => {
