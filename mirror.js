118a24a3ef5e8618d2cdb62b6b9c1eeea06502d8
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-04-08 17:09:47 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -84,7 +84,7 @@ client.on('message', (message) => {
 client.on('message', message => {
   if (message.content === '911') {
     let mod = new Discord.RichEmbed()
-      .setAuthor(message.author.username. message.author.displayAvatarURL)
+      .setAuthor(message.author.username, message.author.displayAvatarURL)
       .setDescription("<@" + message.author.id + ">" + "Вызывает <@&618128028748349450>")
       .setColor("#8b00ff")
       .setTimestamp()
