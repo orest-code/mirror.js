e76a07444c4a6b40e2de8fa5a163e15c9310932f
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-04-14 22:00:36 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js'); 
 const config = require('./config.json');
 const ms = require("ms");
-var guildConf = require('./guildConf.json');
 const fs = require('fs');
 
 
@@ -88,33 +87,13 @@ client.on('message', message => {
 }); 
 
 
-//lol
-bot.on('guildCreate', (guild) => { // If the Bot was added on a server, proceed
-    if (!guildConf[guild.id]) { // If the guild's id is not on the GUILDCONF File, proceed
-	guildConf[guild.id] = {
-		prefix: config.prefix
-	}
-    }
-     fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-     	if (err) console.log(err)
-	})
-});
-
-
-bot.on('guildDelete', (guild) => { // If the Bot was removed on a server, proceed
-     delete guildConf[guild.id]; // Deletes the Guild ID and Prefix
-     fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-     	if (err) console.log(err)
-	})
-});
-
-
 //commands
-client.on('message', (message) => {
-    if (message.channel.type === "dm" || message.author.bot || message.author === bot.user) return; // Checks if we're on DMs, or the Author is a Bot, or the Author is our Bot, stop.
-    var args = message.content.split(' ').slice(1); // We need this later
-    var command = message.content.split(' ')[0].replace(guildConf[message.guild.id].prefix, ''); // Replaces the Current Prefix with this
-  
+client.on("message", async message => {  
+  if(message.author.bot) return; 
+  if(message.content.indexOf(process.env.PREFIX) !== 0) return; 
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
 
   //help command
     if(command === "help") {
