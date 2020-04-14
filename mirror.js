c268d8cf0c57aaa82ef8f02e17abeafa43480a86
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-04-14 21:16:15 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -150,8 +150,8 @@ client.on("message", async message => {
 
     if(command === "prefix") {
 	if (args[0]) {
-		database.getGuildData(msg.guild).prefix = args[0];
-		await msg.channel.send(`Новый префикс для команд: '${args[0]}'`);
+		database.getGuildData(message.guild).prefix = args[0];
+		await message.channel.send(`Новый префикс для команд: '${args[0]}'`);
 	}
 }
     
