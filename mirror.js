d6a387c805de0c7a5e2e0bc7e128f06f1045ee31
DresserPrince <53398000+DresserPrince@users.noreply.github.com>
2020-04-15 13:31:12 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -135,6 +135,16 @@ client.on("message", async message => {
 }
 
 
+//owners list
+    if (command === "owners") {
+              const moderators = new Discord.RichEmbed()
+                .setColor("#8b00ff")
+                .setTitle('Список владельцев сервера:')
+                .setDescription(message.guild.roles.get('527027574212395018').members.map(m => m.user.tag).join('\n'));
+              message.channel.send(moderators);
+            }
+
+
   //moderators list
     if (command === "moderators") {
               const moderators = new Discord.RichEmbed()
