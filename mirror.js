4801ce8b973b22cdb45af38b5da1dff84bd8fcf5
Orest <53398000+orest-code@users.noreply.github.com>
2020-05-17 22:57:53 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -139,16 +139,6 @@ client.on("message", async message => {
 }
 
 
-  //moderators list
-   if (command === "moderators") {
-   const mods = new Discord.RichEmbed()
-          .setColor("#8b00ff")
-          .setTitle('Список модераторов сервера:')
-          .setDescription(message.guild.roles.get('618128028748349450').members.map(m => m.user.tag).join('\n'));
-   message.channel.send(mods);
-}
-
-
   //random number command
     if(command === "random") {
     const lol = args.join(" ");
